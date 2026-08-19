84c9bd7e82a02757c1cf4ac6ce73c8e8b2491440
Rahim-Ahmed-10 <rahimahmed01690@gmail.com>
2026-08-20 02:15:31 +0600

    ADD: Added by Footer And ProductVisionQuote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Banner from "@/components/Banner/Banner";
+import Footer from "@/components/Footer/Footer";
+import ProductVision from "@/components/ProductVisionQuote/ProductVisionQuote";
 
 const Page = () => {
   return (
     <div>
       <Banner />
+      <ProductVision />
+      <Footer />
     </div>
   );
 };
